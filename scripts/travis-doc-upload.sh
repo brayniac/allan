f9cd2349f92bc90654a96c52f780a8540bc806cc
Brian Martin <brayniac@gmail.com>
2016-08-12 17:12:26 +0000

    rotate autodoc key

--- scripts/travis-doc-upload.sh
+++ scripts/travis-doc-upload.sh
@@ -5,10 +5,6 @@
 
 set -e
 
-cargo doc
-
-. ./scripts/doc.cfg
-
 [ "$TRAVIS_BRANCH" = master ]
 
 [ "$TRAVIS_OS_NAME" = linux ]
@@ -17,11 +13,15 @@ cargo doc
 
 [ "$TRAVIS_PULL_REQUEST" = false ]
 
+cargo doc
+
+. ./scripts/doc.cfg
+
 eval key=\$encrypted_${SSH_KEY_TRAVIS_ID}_key
 eval iv=\$encrypted_${SSH_KEY_TRAVIS_ID}_iv
 
 mkdir -p ~/.ssh
-openssl aes-256-cbc -K $key -iv $iv -in scripts/travis.enc -out ~/.ssh/id_rsa -d
+openssl aes-256-cbc -K $key -iv $iv -in scripts/autodoc.enc -out ~/.ssh/id_rsa -d
 chmod 600 ~/.ssh/id_rsa
 
 git clone --branch gh-pages git@github.com:$DOCS_REPO deploy_docs
